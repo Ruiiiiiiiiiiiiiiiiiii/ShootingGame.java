a34e882e775e98a09ed3df4f1a71a0a222d695a2
rui <rui101304@gmail.com>
2024-06-17 11:36:51 +0900

    練習問題10-1 10-2

--- shooting/src/Player.java
+++ shooting/src/Player.java
@@ -14,7 +14,7 @@ public class Player extends Character implements KeyListener {
 			vx=5;
 		}
 		if(e.getKeyCode()==KeyEvent.VK_SPACE) {
-			GameWorld.playerBullets.add(new PlayerBullet(x,y,0,-10));
+			GameWorld.playerBullets.add(new PlayerBullet(x,y,9,-10));
 			System.out.println("弾の数="+
 			GameWorld.playerBullets.size());
 		}
