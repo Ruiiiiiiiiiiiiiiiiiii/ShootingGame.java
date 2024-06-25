db0559a57bb18c152c35ca58e4f0b45d6d3a0940
rui <rui101304@gmail.com>
2024-06-25 11:11:17 +0900

    練習問題13-1

--- shooting/src/EnemyBase.java
+++ shooting/src/EnemyBase.java
@@ -2,7 +2,7 @@
 public class EnemyBase extends Enemy{
 	public EnemyBase(double x,double y,double vx,double vy) {
 		super(x,y,vx,vy);
-		life=20;
+		life=19+GameWorld.stage;
 		score=10;
 	}
 	public void move() {
@@ -13,13 +13,13 @@ public class EnemyBase extends Enemy{
 			GameWorld.enemies.add(new StraightEnemy(x,y,0,1+GameWorld.stage));
 		}
 		if(Math.random()<0.05) {
-			GameWorld.enemies.add(new RandomEnemy(x,y,0,GameWorld.stage));
+			GameWorld.enemies.add(new RandomEnemy(x+GameWorld.stage,y,0,GameWorld.stage));
 		}
 		if(Math.random()<0.05) {
-			GameWorld.enemies.add(new DropEnemy(x,y,0,GameWorld.stage));
+			GameWorld.enemies.add(new DropEnemy(x,y+GameWorld.stage,0,GameWorld.stage));
 		}
 		if(Math.random()<0.05) {
-			GameWorld.enemies.add(new CurveEnemy(x,y,0,GameWorld.stage));
+			GameWorld.enemies.add(new CurveEnemy(x+GameWorld.stage,y,0,GameWorld.stage));
 		}
 	}
 	public void draw(MyFrame f) {
